d736928bb22be9fbb35891a7311608eb5a5e9136
Naruth Kongurai <contact@naruthk.com>
2017-11-25 22:41:01 -0800

    Fixed documentation

--- README.md
+++ README.md
@@ -7,16 +7,18 @@ The current project is under development.
 <!-- TOC -->
 
 - [UW Food App](#uw-food-app)
-  - [Our Inspiration](#our-inspiration)
-  - [Why Developed It](#why-developed-it)
-  - [Developers](#developers)
-  - [Project Pitch Documents](#project-pitch-documents)
-    - [Chat](#chat)
+  - [About The Project](#about-the-project)
+    - [Our Inspiration](#our-inspiration)
+    - [Why Developed It](#why-developed-it)
+    - [Developers](#developers)
+    - [Project Pitch Documents](#project-pitch-documents)
   - [Development](#development)
+    - [Chat](#chat)
     - [Requirements](#requirements)
     - [Installation](#installation)
     - [Libraries We're Using](#libraries-were-using)
-  - [Database Documentation](#database-documentation)
+    - [Testing](#testing)
+  - [Documentation](#documentation)
     - [JSON](#json)
       - [Restaurants.JSON](#restaurantsjson)
       - [Menu.JSON](#menujson)
@@ -27,36 +29,36 @@ The current project is under development.
 
 ----
 
-## Our Inspiration
+## About The Project
+
+### Our Inspiration
 
 We’re inspired by the simplicity and convenience provided by established mobile applications such as Yelp and OneBusAway (UW-owned). Yelp offers users the ability to look up restaurants near them, read reviews, provide ratings, and see information on each restaurant. On the other hand, OneBusAway makes it easy for users to check estimated bus wait times. It’s fast, reliable, and easy to use.
 
-## Why Developed It
+### Why Developed It
 
 While OneBusAway allows us to find bus routes and wait times at each bus stop, we’re building a mobile application that allows us to search for restaurants, food items, prices, popular hours, and lots more. If you’re registered, you can also provide reviews for a restaurant.
 
-## Developers
+### Developers
 
 We’re a team of three funny, talkative, and cheerful individuals: [Naruth Kongurai](http://www.naruthk.com), Demi Tu, and Thipok Cholsaipant.
 
-----
-
-## Project Pitch Documents
+### Project Pitch Documents
 
 - Google Doc: [Pitch Deck](https://docs.google.com/presentation/d/1MVVqnKYfs7XXRjwEztFYqFT89grxo69O_V0npmqn-4U/edit#slide=id.g2891f1afb5_0_83)
 - Google Slide: [Pitch Presentation](https://docs.google.com/document/d/1E4Wk3MKEe6RLPMeRxuAHHm1br9HlJo3zwEEgsLuBOeQ)
-
-### Chat
-
-- Slack application: [uwfoodappproject.slack.com](uwfoodappproject.slack.com)
 
 ----
 
 ## Development
 
+### Chat
+
+- Slack: [uwfoodappproject.slack.com](uwfoodappproject.slack.com)
+
 ### Requirements
 
-We will need CocoaPods installed in order for the UW Food App to work. So if you don't have CocoaPods installed in your computer, run the following commands in Terminal:
+You will need CocoaPods to be installed in your computer in order for the the mobile application to work. So if you don't have CocoaPods installed in your computer, run the following commands in Terminal:
 
 ```text
 sudo gem install cocoapods
@@ -72,7 +74,7 @@ git clone https://github.com/naruthk/mobile-uw-food-app.git
 pod install
 ```
 
-Now to start developing the app, open the file **`UW Food App.xcworkspace`**.
+You must open **`UW Food App.xcworkspace`** in order to start developing the application. Instead, if you open the other file that contains the `.xcodeproj` extension, you might NOT be able to properly build and run the app.
 
 ### Libraries We're Using
 
@@ -81,9 +83,13 @@ Now to start developing the app, open the file **`UW Food App.xcworkspace`**.
 - SwiftyJSON
 - Alamofire
 
+### Testing
+
+Unit testings to be written at a later point.
+
 ----
 
-## Database Documentation
+## Documentation
 
 ### JSON
 
@@ -93,11 +99,13 @@ There are three JSON files that the app retrieves data from.
 - Menus.JSON
 - Reviews.JSON
 
+All files are currently hosted on my website: [http://naruthk.com/api/mobile-uw-food-app/data/Restaurants.json](http://naruthk.com/api/mobile-uw-food-app/data/Restaurants.json). Alternatively, they are available for viewing inside this Github repository's [data](/data) folder.
+
+Eventually, we will need to migrate all of our data to Firebase, so that information can be retrieved and updated and can be reflected in the application immediately.
+
 In every JSON file, there is a shared `restaurantID` for each restaurant, meaning that even though there are 3 seperate JSON files, we can still easily lookup basic information (such as contact information, description), menu, and reviews of a single restaurant by using the unique `restaurantID`.
 
 When adding data to the JSON files, make sure that the `restaurantID` for each restaurant is the same in all 3 JSON files.
-
-All files are currently hosted on my website: [http://naruthk.com/api/mobile-uw-food-app/data/Restaurants.json](http://naruthk.com/api/mobile-uw-food-app/data/Restaurants.json). Alternatively, they are available for viewing inside this Github repository's [data](/data) folder.
 
 #### Restaurants.JSON
 
@@ -114,8 +122,10 @@ View actual `Restaurants.json` data file [here](data/Restaurants.json).
 | category | Text `string` | Category of the restaurant |
 | averageRating | Text `string` | Rating for this restaurant |
 | hours | Dictionary `string:string` | Operation hours from Monday until Sunday |
-| popularHours | Dictionary `string:string` | Popular hours from Monday until Sunday |
+| popularHours * | Dictionary `string:string` | Popular hours from Monday until Sunday |
 | contactInformation | Dictionary `string:string` | Name, phone number, website, and email address |
+
+Note: *`*` Popular hours are mocked data.*
 
 ----
 
--- UW Food App/Controller/HomeViewController.swift
+++ UW Food App/Controller/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  UW Food App
 //
 //  Created by Naruth Kongurai on 11/23/17.
@@ -30,73 +30,45 @@ class HomeViewController: UIViewController, CLLocationManagerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        initializeLocationManager()
-        
+        initializeRestaurantsData()
+        // initializeLocationManager()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-        
     }
     
-    // LocationManager finds out where the user's location is. It asks for the user's permission
-    // to get access to the phone's GPS system. Once permission is given, it looks up the current
-    // location (best accuracy location) -- note that this drains the battery!
-    func initializeLocationManager() {
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.startUpdatingLocation()
-    }
-    
-    // Fetches information of all restaurants and also look up a distance from the user's current
-    // location and each restaurant's location using the Google Map Distance Matrix API. This method
-    // also appends each Restaurant object to the global "restaurantsData" List.
-    func initializeRestaurantsData(url: String, userOriginsLocation: String) {
-        
-        print("DEBUG: Initializing Restaurants Data")
-            
-        // Retrieve Restaurants.JSON Data using Alamo
-        Alamofire.request(url, method: .get).responseJSON { response in
+    // Initializes the View by fetching and populating view with each restaurant's data
+    func initializeRestaurantsData() {
+        print("DEBUG: START => Fetching JSON for Restaurants")
+        Alamofire.request(DATA_RESTAURANTS_URL, method: .get).responseJSON { response in
             if response.result.isSuccess {
-                
-                let restaurantsJSON : JSON = JSON(response.result.value!)
-                self.updateRestaurantsData(json: restaurantsJSON)
-                
+                let json : JSON = JSON(response.result.value!)
+                print("DEBUG: Length of results: \(json.count)")
+                let results = json["appData"]["restaurants"]
+                for currentRestaurant in results.arrayValue {
+                    let restaurantID = currentRestaurant["restaurantID"].string
+                    let restaurant = self.fetchCurrentRestaurantData(currentRestaurant: currentRestaurant)
+                    self.restaurantsData[restaurantID!] = restaurant
+                }
             } else {
                 print("Error \(String(describing: response.result.error))")
-                // TODO: - Alert the user that the connection is offline
-                
             }
         }
     }
-    
-    // This method looks up information of each restaurant inside the JSON object and append each restaurant
-    // data to the global list.
-    func updateRestaurantsData(json : JSON) {
-        let results = json["appData"]["restaurants"]
-        
-        print("DEBUG: Length of results: \(results.count)")
-        
-        for currentRestaurant in results.arrayValue {
-            let restaurantObject = fetchCurrentRestaurantData(currentRestaurant: currentRestaurant)
-            let restaurantID = currentRestaurant["restaurantID"].string
-            restaurantsData[restaurantID!] = restaurantObject
-        }
-    }
-    
+
     func fetchCurrentRestaurantData(currentRestaurant: JSON) -> Restaurant {
-        
-        print("DEBUG: Fetching Current Restaurant Data")
-        
         let restaurantID = currentRestaurant["restaurantID"].string
+        print("DEBUG: START => Fetching data of restaurant ID \(restaurantID ?? "")")
         let name = currentRestaurant["name"].string
         let restaurantDescription = currentRestaurant["description"].string
         let locationName = currentRestaurant["locationName"].string
         let fullAddress = currentRestaurant["fullAddress"].string
         let category = currentRestaurant["category"].string
         let averageRating = currentRestaurant["averageRating"].string
+        let latitude = "\(currentRestaurant["mapCoordinates"]["latitude"].string ?? "")"
+        let longitude = "\(currentRestaurant["mapCoordinates"]["longitude"].string ?? "")"
+        let mapCoordinates = [latitude, longitude] as [String]
         let contact_name = currentRestaurant["contactInformation"]["name"].string
         let contact_email = currentRestaurant["contactInformation"]["email"].string
         let contact_phone = currentRestaurant["contactInformation"]["phone"].string
@@ -116,20 +88,8 @@ class HomeViewController: UIViewController, CLLocationManagerDelegate {
         hours.append(["fri": hourFri!])
         hours.append(["sat": hourSat!])
         hours.append(["sun": hourSun!])
-    
-        // Map Coordinates
-        let latitude = "\(currentRestaurant["mapCoordinates"]["latitude"].string ?? "")"
-        let longitude = "\(currentRestaurant["mapCoordinates"]["longitude"].string ?? "")"
-        let mapCoordinates = [latitude, longitude] as [String]
         
-        print("DEBUG: Current restaurant's coordinates are \(mapCoordinates)")
-        
-        // Find out how far this current restaurant is from the user.
-        fetchAndAddRelativeDistancesToCurrentRestaurant(currentRestaurantID: restaurantID!, destinationLatitude: latitude, destinationLongitude: longitude)
-        
-        print("DEBUG: Finish fetching current restaurant data")
-        
-        return Restaurant(
+        let restaurant = Restaurant(
             restaurantID: restaurantID!,
             name: name!,
             restaurantDescription: restaurantDescription!,
@@ -145,52 +105,23 @@ class HomeViewController: UIViewController, CLLocationManagerDelegate {
             contact_website: contact_website!,
             relativeDistanceFromUserCurrentLocation: "-",
             relativeDurationFromUserCurrentLocation: "-")
+        
+        print("DEBUG: END => Completed fetching data of current restaurant")
+        print("DEBUG: DATA => \(restaurant.description)")
+        print("DEBUG: --------")
+        
+        return restaurant
     }
     
-    func fetchAndAddRelativeDistancesToCurrentRestaurant(currentRestaurantID: String, destinationLatitude: String, destinationLongitude: String) {
-        
-        print("DEBUG: Getting relative distances from the user's current location")
-        
-        let parameters : [String: String] = [
-            "origins" : userOriginsLocation,
-            "destinations" : "\(destinationLatitude),\(destinationLongitude)",
-            "mode" : "walking",
-            "key": GOOGLE_MAP_DISTANCE_MATRIX_API_KEY]
-
-        Alamofire.request(GOOGLE_MAP_DISTANCE_URL, method: .get, parameters: parameters).responseJSON { response in
-            if response.result.isSuccess {
-                
-                print("DEBUG: Obtaining data from Google Map API")
-                
-                let result : JSON = JSON(response.result.value!)
-                for currentElement in result["rows"][0]["elements"].arrayValue {
-                    
-                    let status = currentElement["status"].string
-                    
-                    print("DEBUG: Digging inside currentElement's JSON")
-                    print("DEBUG: API status - \(status ?? "")")
-
-                    if (status == "OK") {
-                        
-                        print("DEBUG: Updating current restaurant object with new relative distancea and duration")
-                        
-                        let locationDistance = "\(currentElement["distance"]["text"].string ?? "")"
-                        let locationDuration = "\(currentElement["duration"]["text"].string ?? "")"
-                        self.restaurantsData[currentRestaurantID]?.updateRelativeDistancesAndDuration(newDistance: locationDistance, newDuration: locationDuration)
-                    } else {
-                        self.locationDistanceText = "-"
-                        self.locationDurationText = "-"
-                    }
-                }
-            } else {
-                print("Error \(String(describing: response.result.error))")
-                // TODO: - Alert the user that the connection is offline
-                
-            }
-            print("DEBUG: Done obtaining data from Google Map API")
-            print("DEBUG: Data for Restaurant ID \(currentRestaurantID): \(self.restaurantsData[currentRestaurantID]?.description ?? "")")
-            print("DEBUG: End Alomafire session")
-        }
+    // LocationManager finds out where the user's location is. It asks for the user's permission
+    // to get access to the phone's GPS system. Once permission is given, it looks up the current
+    // location (best accuracy location) -- note that this drains the battery!
+    func initializeLocationManager() {
+        print("DEBUG: START => Looking up current location")
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
     
     //MARK: - Location Manager Delegate Methods
@@ -206,20 +137,55 @@ class HomeViewController: UIViewController, CLLocationManagerDelegate {
             let latitude = String(location.coordinate.latitude)
             let longitude = String(location.coordinate.longitude)
             userOriginsLocation = "\(latitude),\(longitude)"
+            print("DEBUG: END => Found current location: \(userOriginsLocation)")
             
-            print("DEBUG: Current user location: \(userOriginsLocation)")
-            
-            initializeRestaurantsData(url: DATA_RESTAURANTS_URL, userOriginsLocation: userOriginsLocation)
+            setRelativeDistancesForEachRestaurant(userOriginsLocation: userOriginsLocation)
         }
     }
     
-    // Report an error if we cannot find the user's location
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print(error);
-        // TODO: - Show some alert to the user
-        
+    func setRelativeDistancesForEachRestaurant(userOriginsLocation: String) {
+        print("DEBUG: START => Trying to set relative distances for each restaurant. There are \(restaurantsData.values.count) total.")
+        for restaurant in restaurantsData.values {
+            print("DEBUG: Setting values for restaurant ID \(restaurant.restaurantID)")
+            let restaurantID = restaurant.restaurantID
+            let latitude = restaurant.mapCoordinates[0]
+            let longitude = restaurant.mapCoordinates[1]
+            let parameters : [String: String] = [
+                "origins" : userOriginsLocation,
+                "destinations" : "\(latitude),\(longitude)",
+                "mode" : "walking",
+                "key": GOOGLE_MAP_DISTANCE_MATRIX_API_KEY]
+            
+            Alamofire.request(GOOGLE_MAP_DISTANCE_URL, method: .get, parameters: parameters).responseJSON { response in
+                if response.result.isSuccess {
+                    print("DEBUG: START => Obtaining data from Google Map Distance API")
+                    let result : JSON = JSON(response.result.value!)
+                    for currentElement in result["rows"][0]["elements"].arrayValue {
+                        let status = currentElement["status"].string
+                        print("DEBUG: CHECK => API status - \(status ?? "")")
+                        if (status == "OK") {
+                            print("DEBUG: PROGRESS => Updating current restaurant object with new relative distancea and duration")
+                            let locationDistance = "\(currentElement["distance"]["text"].string ?? "")"
+                            let locationDuration = "\(currentElement["duration"]["text"].string ?? "")"
+                            self.restaurantsData[restaurantID]?.updateRelativeDistancesAndDuration(newDistance: locationDistance, newDuration: locationDuration)
+                            print("DEBUG: END => Restaurant ID \(restaurantID) has been updated")
+                        } else {
+                            self.locationDistanceText = "-"
+                            self.locationDurationText = "-"
+                        }
+                    }
+                } else {
+                    print("Error \(String(describing: response.result.error))")
+                }
+                print("DEBUG: DATA => Data for Restaurant ID \(restaurantID): \(self.restaurantsData[restaurantID]?.description ?? "")")
+                print("DEBUG: COMPLETED => End Alomafire session")
+            }
+        }
     }
 
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error);
+    }
 
 }
 
